fa7733cc5f2a08131454504a1976a01f941f3d48
Marcus Fernando Correa Barbosa <marcus.barbosa.ext@rendimento.com.br>
2019-06-11 14:28:00 -0300

    Adiconando Tooltip nos botoes do grid

--- 0_App/ProAgil-App/src/app/app.module.ts
+++ 0_App/ProAgil-App/src/app/app.module.ts
@@ -1,22 +1,23 @@
-import { DateTimeFormatPipePipe } from './_helps/DateTimeFormatPipe.pipe';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { HttpClientModule } from '@angular/common/http';
-
 import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+
 // ngx-Bootstrap
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ModalModule } from 'ngx-bootstrap/modal';
 // ngx-Bootstrap
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { NavComponent } from './nav/nav.component';
+
 import { EventoService } from './_services/evento.service';
 
+import { DateTimeFormatPipePipe } from './_helps/DateTimeFormatPipe.pipe';
 
 @NgModule({
    declarations: [
