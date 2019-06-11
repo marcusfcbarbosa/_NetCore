bec4ca37c2bf5a943f9257cde9c6d8a3ad4ca80e
Marcus Fernando Correa Barbosa <marcus.barbosa.ext@rendimento.com.br>
2019-06-11 10:40:00 -0300

    Ajustando Layouts

--- 0_App/ProAgil-App/src/app/_services/evento.service.ts
+++ 0_App/ProAgil-App/src/app/_services/evento.service.ts
@@ -20,9 +20,8 @@ export class EventoService {
   getEventoById(id: number): Observable<Evento> {
     return this.http.get<Evento>(`${this.baseURL}/${id}`);
   }
-  
+
   getEventoByIdentifier(identifier: string): Observable<Evento> {
     return this.http.get<Evento>(`${this.baseURL}/${identifier}`);
   }
-
 }
