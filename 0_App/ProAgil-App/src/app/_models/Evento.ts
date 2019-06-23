a166919c53ccb56c990aa04c935cace7be7abaac
marcus fernando <marcusfcbarbosa@gmail.com>
2019-06-23 20:48:32 -0300

    validação de forms com a diretiva  [ngClass]="{'is-invalid':

--- 0_App/ProAgil-App/src/app/_models/Evento.ts
+++ 0_App/ProAgil-App/src/app/_models/Evento.ts
@@ -1,18 +1,17 @@
-import { Lote } from './Lote';
-import { Palestrante } from './Palestrante';
-import { RedeSocial } from './RedeSocial';
+import { Lote } from "./Lote";
+import { Palestrante } from "./Palestrante";
+import { RedeSocial } from "./RedeSocial";
 
 export interface Evento {
-
-    local: string;
-    dataEvento: Date;
-    tema: string;
-    qtdPessoas: number;
-    imgUrl: string;
-    telefone: string;
-    identifyer: string;
-    email: string;
-    lotes: Lote[];
-    palestranteEventos: Palestrante[];
-    redesSociais: RedeSocial[];
+  local: string;
+  dataEvento: Date;
+  tema: string;
+  qtdPessoas: number;
+  imgUrl: string;
+  telefone: string;
+  identifyer: string;
+  email: string;
+  lotes: Lote[];
+  palestranteEventos: Palestrante[];
+  redesSociais: RedeSocial[];
 }
